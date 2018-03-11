ff67d3c1b7ab9eb419c230b13dcc3831e7d0d164
sapphirekuo <sapphirekuo@gmail.com>
2018-03-11 16:06:37 +0800

    setup routes for user, generate user controller, and add user show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
-  , controllers: { registrations: 'users/registrations' }
+  devise_for :users, controllers: { registrations: 'users/registrations' }
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "paths#index" 
+  resources :users, only: [:show]
 
 
   namespace :admin do
