d61dc6a232345d9f023cfea1b83097ecea339b91
anna770822 <anna770822@hotmail.com>
2018-03-13 10:28:37 +0800

    routes init

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,16 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "trips#index"
+  root "gostations#index"
+  
   resources :users, only: [:show]
+   
+  resources :gostations, only:[:index]
+
+  resources :trips, only:[:index, :show]
 
   namespace :admin do
-    root "trips#index"
+    root "gostations#index"
   end
 
 
