5e476e043d1e4693b4b9ae79fec644b56f2c618d
anna770822 <anna770822@hotmail.com>
2018-03-13 12:00:29 +0800

    no message

--- app/controllers/trip_gostations_controller.rb
+++ app/controllers/trip_gostations_controller.rb
@@ -1,6 +1,6 @@
 class TripGostationsController < ApplicationController
   def check
-
+ 
     @trip_gostation = TripGostation.find(params[:id])
     @trip_gostation.update(status: !(@trip_gostation.status))
 
