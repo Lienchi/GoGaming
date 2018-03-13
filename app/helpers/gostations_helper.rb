42c727750bee7b871ee016d91f14fd98a21f69b7
anna770822 <anna770822@hotmail.com>
2018-03-13 10:39:45 +0800

    link pages together root page/user#show/trips#index

---
+++ app/helpers/gostations_helper.rb
@@ -0,0 +1,2 @@
+module GostationsHelper
+end
---
+++ test/controllers/gostations_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class GostationsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
