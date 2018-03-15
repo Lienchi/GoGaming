5e3c0f4091132fb67126a9d0873912e519ff50f8
anna770822 <anna770822@hotmail.com>
2018-03-15 10:53:13 +0800

    admin trip CRUD

--- app/controllers/admin/trips_controller.rb
+++ app/controllers/admin/trips_controller.rb
@@ -1,7 +1,7 @@
 class Admin::TripsController < ApplicationController
   before_action :authenticate_user!
   before_action :authenticate_admin
-  
+  before_action :set_trip, only:[:update, :destroy]
   def index
     @trips = Trip.all
 
@@ -10,5 +10,38 @@ class Admin::TripsController < ApplicationController
     else
       @trip = Trip.new
     end
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    if @trip.save
+      flash[:notce] = "已儲存"
+      redirect_to admin_trips_path
+    else
+      flash.now[:alert] = "儲存失敗"
+      render :new
+    end
+  end
+  
+  def update
+
+     @trip.update(trip_params)
+     redirect_to admin_trips_path
+  end
+
+  def destroy
+
+     @trip.delete
+     redirect_to admin_trips_path
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:name, :gostations_index)
+  end
+
+  def set_trip
+     @trip = Trip.find(params[:id])
   end
 end
