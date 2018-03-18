2b1555a5def3726a499947b4ad3cfacdb3f0d81d
Lienchi <Lienchi.Lai@gmail.com>
2018-03-18 17:29:54 +0800

    display availableTime for gostation and display gostations nearby destination

--- app/controllers/gostations_controller.rb
+++ app/controllers/gostations_controller.rb
@@ -1,6 +1,8 @@
 class GostationsController < ApplicationController
   def index
     @user = current_user
-    gon.gostations = Gostation.all
+    @trip_gostations = TripGostation.where(user_id: current_user.id).uniq
+    gon.gostations_belongs_to_trip = Gostation.where(id: @trip_gostations.map(&:gostation_id))
+    gon.gostations_not_belongs_to_trip = Gostation.where.not(id: @trip_gostations.map(&:gostation_id))
   end
 end
