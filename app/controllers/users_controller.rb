3962e36401e88297fb18695208af727091e397d5
anna770822 <anna770822@hotmail.com>
2018-03-22 13:34:44 +0800

    add :avatar to user , add uploader

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,22 @@ class UsersController < ApplicationController
     @user = current_user
   end
 
+  def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to user_path
+
+  end
+
   def leaderboards
     @scores = Merit::Score.top_scored
     @friendscores = Merit::Score.top_scored
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:avatar)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180317071448) do
+ActiveRecord::Schema.define(version: 20180322043432) do
 
   create_table "badges_sashes", force: :cascade do |t|
     t.integer "badge_id"
@@ -129,6 +129,7 @@ ActiveRecord::Schema.define(version: 20180317071448) do
     t.datetime "updated_at", null: false
     t.integer "sash_id"
     t.integer "level", default: 0
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
