d75aa5fe13f74f9a7be25659710395ab03a318c8
Lienchi <Lienchi.Lai@gmail.com>
2018-03-30 00:54:45 +0800

    add side page for trip#show page

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -12,6 +12,7 @@ class TripsController < ApplicationController
     @trip = Trip.find(params[:id])
     @trip_gostations = TripGostation.where(trip_id: @trip.id, user_id: current_user.id)
     gon.gostations = Gostation.where(id: @trip_gostations.map(&:gostation_id))
+    gon.trip_gostations = @trip_gostations
     @comment = Comment.new
     @comments = Comment.where(trip_id: @trip).order(created_at: :desc)
   end
