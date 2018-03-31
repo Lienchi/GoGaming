9e3d9807b346a8833262e7aaaca52f3824254fd1
anna770822 <anna770822@hotmail.com>
2018-03-31 23:10:14 +0800

    add gem fog and set configuration

--- app/uploaders/comment_uploader.rb
+++ app/uploaders/comment_uploader.rb
@@ -13,6 +13,12 @@ class CommentUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
