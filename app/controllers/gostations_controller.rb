005a27293e5ebc9b5a730c293a20d3de985534b7
Lienchi <Lienchi.Lai@gmail.com>
2018-04-04 12:25:21 +0800

    display battery icon by power status

--- app/controllers/gostations_controller.rb
+++ app/controllers/gostations_controller.rb
@@ -4,9 +4,7 @@ class GostationsController < ApplicationController
   def index
     @user = current_user
     @trip_gostations = TripGostation.where(user_id: current_user.id).uniq
-    gon.gostations_belongs_to_trip = Gostation.where(id: @trip_gostations.map(&:gostation_id))
-    gon.gostations_not_belongs_to_trip = Gostation.where.not(id: @trip_gostations.map(&:gostation_id))
-    gon.trip_gostations = @trip_gostations
+    gon.gostations = Gostation.all
     gon.friendly_stores = Friendlystore.all
   end
 
