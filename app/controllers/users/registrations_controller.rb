bdcdb0972cc8e11820891f8f0c78303dfcdc048a
anna770822 <anna770822@hotmail.com>
2018-04-04 20:07:15 +0800

    add before_action :configure_permitted_parameters in order to move :avatar to devise registration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,24 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   after_action :create_trip_gostation, only: :create
+  before_action :configure_permitted_parameters
+ 
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+    user_params.permit(:name,
+    :email, :password, :password_confirmation, :avatar)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |user_params|
+   user_params.permit(:name,
+    :email, :password, :password_confirmation, :current_password, :avatar)
+    end
+  end
+
+ 
+
+
+
+
   def create
     @user = build_resource # Needed for Merit
     super
