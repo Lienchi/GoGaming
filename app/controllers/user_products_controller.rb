dbd4efb279307799e5a12ae5c9b450effbab43dc
anna770822 <anna770822@hotmail.com>
2018-04-09 11:34:33 +0800

    change prompt message / set @repairstore not save to  flash message

--- app/controllers/user_products_controller.rb
+++ app/controllers/user_products_controller.rb
@@ -1,10 +1,16 @@
 class UserProductsController < ApplicationController
 
+
   def create
     @product = Product.find(params[:product_id])
-    @repairstore = Repairstore.find(params[:user_product][:repairstore_id])
+    @repairstore = Repairstore.find_by_id(params[:user_product][:repairstore_id])
     @user_product = @product.user_products.where(user_id: current_user).new(user_product_params)
-    if current_user.points > @product.product_points
+   
+    if @repairstore.blank?
+      redirect_to products_path
+      flash[:alert]="未選擇取貨店家"
+    elsif
+      current_user.points > @product.product_points
       @user_product.save!
       current_user.subtract_points(@product.product_points)
       UserMailer.notify_order_create(@user_product).deliver_now!
