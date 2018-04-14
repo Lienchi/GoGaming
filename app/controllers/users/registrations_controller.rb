806951042d72f24fb7d4cde620b81e42687ebff8
Lienchi <Lienchi.Lai@gmail.com>
2018-04-14 13:03:04 +0800

    create trip_gostation record for new registration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,4 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  after_action :create_trip_gostation, only: :create
   before_action :configure_permitted_parameters
  
 
@@ -25,19 +24,5 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def update
     @user = resource # Needed for Merit
     super
-  end
-
-  def create_trip_gostation
-
-    Trip.all.each do |t|
-      t.gostations_index.each do |gid|
-        TripGostation.create!(
-            user_id: @user.id,
-            trip_id: t.id,
-            gostation_id: gid,
-            status: false
-        )
-      end
-    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  after_create :create_trip_gostation
   
   mount_uploader :avatar, PhotoUploader
   has_merit
@@ -91,6 +92,19 @@ class User < ApplicationRecord
       return 2
     else
       return 1
+    end
+  end
+
+  def create_trip_gostation
+    Trip.all.each do |t|
+      t.gostations_index.each do |gid|
+        TripGostation.create!(
+            user_id: self.id,
+            trip_id: t.id,
+            gostation_id: gid,
+            status: false
+        )
+      end
     end
   end
   
